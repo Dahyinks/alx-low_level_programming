dc33839fba9937b2119c899082a99afe9b573cad
dahyinks <olayiwolaolayinka90@gmail.com>
2023-09-11 18:09:26 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that prints a line to the standard error
+ * It return 1 to indicate (Success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
+	stdout);
+return (1);
+}
